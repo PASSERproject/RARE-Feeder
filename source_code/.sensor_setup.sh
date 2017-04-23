e850db07f422c07f920d2661424afc7dcf8182eb
John Filipowicz <Filipowicz@localhost.localdomain>
2017-04-23 19:41:21 -0400

    ...

--- source_code/.sensor_setup.sh
+++ source_code/.sensor_setup.sh
@@ -12,7 +12,8 @@ mkdir -p ~/sensor_libraries/Proximity
 git clone http://github.com/adafruit/Adafruit_Python_DHT ~/sensor_libraries/DHT/
 git clone http://github.com/adafruit/Adafruit_Python_VCNL40xx ~/sensor_libraries/DHT/
 
-cd ~/sensor_libraries
-sudo python ./DHT/setup.py install
-sudo python ./Proximity/setup.py install
-cd ../source_code
+cd ~/sensor_libraries/DHT
+sudo python ./setup.py install
+cd ../Proximity
+sudo python ./setup.py install
+cd ../../source_code
