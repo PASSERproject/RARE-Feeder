9df1b7b01e588e6e5851dfd8319835aece5783b0
John Filipowicz <Filipowicz@localhost.localdomain>
2017-04-23 19:21:52 -0400

    ...

--- source_code/.sensor_setup.sh
+++ source_code/.sensor_setup.sh
@@ -6,11 +6,11 @@
 # Purpose: Run setup for proximity and dht sensors.
 # Note: This must be run before the software's first use on a Pi.
 
-mkdir -p ~/Feeder/sensor_libraries/DHT
-mkdir -p ~/Feeder/sensor_libraries/Proximity
+mkdir -p ~/sensor_libraries/DHT
+mkdir -p ~/sensor_libraries/Proximity
 
-git clone http://github.com/adafruit/Adafruit_Python_DHT ~/Feeder/sensor_libraries/DHT/
-git clone http://github.com/adafruit/Adafruit_Python_VCNL40xx ~/Feeder/sensor_libraries/DHT/
+git clone http://github.com/adafruit/Adafruit_Python_DHT ~/sensor_libraries/DHT/
+git clone http://github.com/adafruit/Adafruit_Python_VCNL40xx ~/sensor_libraries/DHT/
 
-sudo python ~/Feeder/sensor_libraries/DHT/setup.py install
-sudo python ~/Feeder/sensor_libraries/Proximity/setup.py install
+sudo python ~/sensor_libraries/DHT/setup.py install
+sudo python ~/sensor_libraries/Proximity/setup.py install
