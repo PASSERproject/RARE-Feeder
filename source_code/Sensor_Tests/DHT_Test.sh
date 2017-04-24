25732d1c3cdac703b291fabba405474c35c1dea3
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-24 10:11:57 -0400

    Update DHT_Test.sh

--- source_code/Sensor_Tests/DHT_Test.sh
+++ source_code/Sensor_Tests/DHT_Test.sh
@@ -12,7 +12,7 @@ PIN=17
 echo Reading from DHT sensor. May take up to 5 seconds...
 echo
 
-python ~/Documents/feederSoftware/sensor_libraries/DHT_Python_Library/Adafruit_Python_DHT/examples/AdafruitDHT.py 11 "$PIN"
+python ~/sensor_libraries/DHT/examples/AdafruitDHT.py 11 "$PIN"
 
 echo
 echo Temp is read in as Celcius
