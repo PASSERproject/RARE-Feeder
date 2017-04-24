c21374f3c38b19bd9fb1e3c13dfb7b4be2e6927d
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-24 10:14:06 -0400

    Update Proximity_Test.sh

--- source_code/Sensor_Tests/Proximity_Test.sh
+++ source_code/Sensor_Tests/Proximity_Test.sh
@@ -5,4 +5,4 @@
 
 # Purpose: Test the proximity sensor through outputting sensor data
 
-python ~/Documents/feederSoftware/sensor_libraries/Proximity_Python_Library/Adafruit_Python_VCNL40xx/examples/simpletest.py
+python ~/sensor_libraries/Proximity/examples/simpletest.py
