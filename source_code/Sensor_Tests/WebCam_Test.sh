fe4f876681a65d43315ccf5da7961ae8e5906688
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-25 17:45:48 -0400

    Update WebCam_Test.sh

--- source_code/Sensor_Tests/WebCam_Test.sh
+++ source_code/Sensor_Tests/WebCam_Test.sh
@@ -6,4 +6,4 @@
 # Purpose: Take a 10 sec test video from the WebCam (Video Only)
 
 
-streamer -q -r 60 -t 0:0:10 -f rgb24 -o /media/pi/Feeder_Data/test.avi &>/dev/null
+streamer -q -r 60 -t 0:0:10 -f rgb24 -o /media/pi/FEEDER_DATA/test.avi &>/dev/null
