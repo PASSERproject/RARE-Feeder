6e3dd6051615122c84175b88a21cc9e5cf71e944
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-25 16:49:52 -0400

    Add files via upload

---
+++ source_code/servo2.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12,GPIO.OUT)
+freq=50
+pwm = GPIO.PWM(12, freq)
+leftPosition = 0.75
+rightPosition = 2.5
+middlePosition = (rightPosition - leftPosition) /2 + leftPosition
+positionlist = [leftPosition, middlePosition, rightPosition, middlePosition]
+ms=1000/freq
+dutyCyclePercentage=positionlist[0]*100/ms
+pwm.start(dutyCyclePercentage)
+time.sleep(1)
+#dutyCyclePercentage=positionlist[1]*100/ms
+#pwm.start(dutyCyclePercentage)
+#time.sleep(1)
+dutyCyclePercentage=positionlist[2]*100/ms
+pwm.start(dutyCyclePercentage)
+time.sleep(1)
+pwm.stop()
+
+GPIO.cleanup()
