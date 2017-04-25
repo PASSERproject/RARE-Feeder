7309b84de448c9c340ec4a565218257e583fce45
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-25 17:50:23 -0400

    Update images.sh

--- source_code/images.sh
+++ source_code/images.sh
@@ -20,4 +20,4 @@ done
 
 mogrify -resize 800x800 $location/images/*.jpeg
 convert $location/images/*.jpeg -delete 10 -morph 10 $location/images/%05d.jpg
-avconv -r 25 -i $location/images.%05d.jpeg -qscale 2 $location/test.mp4
+avconv -r 25 -i $location/images/%05d.jpeg -qscale 2 $location/test.mp4
