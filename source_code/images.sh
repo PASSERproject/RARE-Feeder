462e90dcfe891dfcadee5cd274d43402b893fc93
John Filipowicz <Filipowicz@localhost.localdomain>
2017-04-26 22:23:50 -0400

    Disabled immediate image processing.

--- source_code/images.sh
+++ source_code/images.sh
@@ -18,6 +18,9 @@ do
 	let i=i+1
 done
 
-mogrify -resize 800x800 $location/images/*.jpeg
-convert $location/images/*.jpeg -delete 10 -morph 10 $location/images/%05d.jpeg
-avconv -r 25 -i $location/images/%05d.jpeg -qscale 2 $location/test.mp4
+
+# Uncomment the below lines if you want to do the image processing immediately.
+
+#mogrify -resize 800x800 $location/images/*.jpeg
+#convert $location/images/*.jpeg -delete 10 -morph 10 $location/images/%05d.jpeg
+#avconv -r 25 -i $location/images/%05d.jpeg -qscale 2 $location/test.mp4
