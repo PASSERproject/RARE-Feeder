8afe74a17d0cc3f73c4dda5964dbe8825296abcd
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-28 10:22:11 -0400

    Update make_videos.sh

--- source_code/make_videos.sh
+++ source_code/make_videos.sh
@@ -9,9 +9,9 @@ location=/media/pi/FEEDER_DATA
 cd location/images
 
 for d in */; do
-  mogrify -resize 800x800 $location/images/$d/*.jpeg
-  convert $location/images/$d/*.jpeg -delete 10 -morph 10 $location/images/$d/%05d.jpeg
-  avconv -r 25 -i $location/images/$d/%05d.jpeg -qscale 2 $location/images/$d/$d.mp4
+  mogrify -resize 800x800 $location/images/$d*.jpeg
+  convert $location/images/$d*.jpeg -delete 10 -morph 10 $location/images/$d%05d.jpeg
+  avconv -r 25 -i $location/images/$d%05d.jpeg -qscale 2 $location/images/$dvideo.mp4
 done
 
 cd ~/Feeder/source_code
