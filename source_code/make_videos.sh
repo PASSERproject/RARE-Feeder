dd4321d298f22bf9c64629ed53a9ce0df6bbdbf0
John Filipowicz <johnfilipowicz14@gmail.com>
2017-04-28 10:19:06 -0400

    Update make_videos.sh

--- source_code/make_videos.sh
+++ source_code/make_videos.sh
@@ -6,9 +6,12 @@
 # Purpose: Create videos for each set of images previously taken.
 
 location=/media/pi/FEEDER_DATA
+cd location/images
 
 for d in */; do
   mogrify -resize 800x800 $location/images/$d/*.jpeg
   convert $location/images/$d/*.jpeg -delete 10 -morph 10 $location/images/$d/%05d.jpeg
   avconv -r 25 -i $location/images/$d/%05d.jpeg -qscale 2 $location/images/$d/$d.mp4
 done
+
+cd ~/Feeder/source_code
