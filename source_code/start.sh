37bf50fc771e8b9b70665c3c0ff09b12161ccce3
cphilson <cphilson@radford.edu>
2017-05-04 17:34:52 -0400

    Update start.sh

--- source_code/start.sh
+++ source_code/start.sh
@@ -14,5 +14,6 @@ fi
 # Remove '#' and the space before it to redirect stderr & stdout to logs
 ./periodic.py & #>./Logs/periodic.log
 ./listener.py & #>./Logs/listener.log
+./programed_feeding.py & 
 #Dr. Ray's Program too
 
